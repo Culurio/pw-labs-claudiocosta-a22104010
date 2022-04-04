f7094f6d64a760cf723708e0ab4978ae031eaf7f
Culurio <claudiohcx@gmail.com>
2022-04-04 17:31:38 +0100

    Tudo feito +-

---
+++ Lab6/pacote/in_out.py
@@ -0,0 +1,16 @@
+def ler_numero():
+    try:
+        int(input('Insira um inteiro entre 1 e 10'))
+    except:
+        print('Não meteu um numero inteiro')
+        
+def imprime_resultados(n,positivo,par):
+    if positivo:
+        positivo = 'positivo'
+    else:
+        positivo = 'negativo'
+    if par:
+        par = 'par'
+    else:
+        par = 'impar'
+    print(f'O numero {n} é {positivo} e {par}')
